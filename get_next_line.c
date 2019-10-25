8cc2bcda3093c346f932bb6eabccf6f86690a41c
David PROVINCIALE <daprovin@e1r10p12.42.fr>
2019-10-25 06:51:09 +0200

    multiple files

---
+++ get_next_line.c
@@ -0,0 +1,149 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daprovin <daprovin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/25 03:04:16 by daprovin          #+#    #+#             */
+/*   Updated: 2019/10/25 06:49:47 by daprovin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "get_next_line.h"
+#include "libft.h"
+
+int		ft_isinlst(int fd, t_statlst **lst, char **dst)
+{
+	t_statlst	*tmp_lst;
+	char		*tmp_dst;
+
+	tmp_lst = *lst;
+	while (tmp_lst)
+	{
+		if (tmp_lst->fd == fd)
+		{
+			tmp_dst = *dst;
+			*dst = ft_strjoin(*dst, tmp_lst->content);
+			free(tmp_dst);
+			return (1);
+		}
+		tmp_lst = tmp_lst->next;
+	}
+	return (0);
+}
+
+void	ft_lstnewback(int fd, t_statlst **lst)
+{
+	t_statlst *new;
+	t_statlst *tmp_lst;
+
+	tmp_lst = *lst;
+	if (!(new = (t_statlst*)malloc(sizeof(t_statlst))))
+		return ;
+	new->fd = fd;
+	new->next = NULL;
+	new->content = ft_strdup("");
+	if (!tmp_lst)
+	{
+		*lst = new;
+		return ;
+	}
+	while (tmp_lst->next)
+		tmp_lst = tmp_lst->next;
+	tmp_lst->next = new;
+}
+
+void	ft_changecontent(int fd, t_statlst **lst,char *dst)
+{
+	t_statlst *tmp_lst;
+	int i;
+	int j;
+
+	tmp_lst = *lst;
+	while (tmp_lst->fd != fd)
+		tmp_lst = tmp_lst->next;
+	i = 0;
+	while (dst[i] != '\n')
+		i++;
+	tmp_lst->content = (char*)malloc(sizeof(ft_strlen(dst) - i));
+	(tmp_lst->content)[ft_strlen(dst) - i - 1] = '\0';
+	j = 0;
+	while (dst[i + 1])
+	{
+		(tmp_lst->content)[j] = dst[i + 1];
+		i++;
+		j++;
+	}
+}
+
+int		ft_get(int fd, char **line)
+{
+	char				BUFFER[11];
+	char				*tmp;
+	char				*dst;
+	static t_statlst	*lst;
+	int					rt;
+	int					i;
+	int					count;
+
+	if (fd < 0)
+		return (-1);
+	dst = ft_strdup("");
+	count = 0;
+	if (ft_isinlst(fd, &lst, &dst))
+		count = 1;
+	
+	while ((rt = read(fd, BUFFER, 10)) > 0)
+	{
+		BUFFER[rt] = '\0';
+		tmp = dst;
+		dst = ft_strjoin(dst, BUFFER);
+		free(tmp);
+		*line = dst;
+		i = 0;
+		while (*(*line + i))
+		{
+			if (*(*line + i) == '\n')
+				*(*line + i) = '\0';
+			i++;
+		}
+		if (ft_isinstr('\n', BUFFER))
+		{
+			if (!count)
+				ft_lstnewback(fd, &lst);
+			ft_changecontent(fd, &lst, BUFFER);
+			break ;
+		}
+	}
+	if (rt == 0)
+	{
+		return (0);
+	}
+	return (1);
+}
+
+int main(int ac, char **av)
+{
+	int fd;
+	char *line;
+	int fd1;
+	int rt;
+
+	ac = 1;
+	line = NULL;
+	fd = open(av[1], O_RDONLY);
+	fd1 = open(av[2], O_RDONLY);
+		rt =ft_get(fd, &line);
+		printf("\033[0;38m%s\n%d\n", line, rt);
+		free(line);
+		rt = ft_get(fd1, &line);
+		printf("\033[0;38m%s\n%d\n", line, rt);
+		rt = ft_get(fd, &line);
+		printf("\033[0;38m%s\n%d\n", line, rt);
+	return 0;
+}
+
--- get_next_line.h
+++ get_next_line.h
@@ -6,8 +6,18 @@
 /*   By: daprovin <daprovin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/23 23:30:37 by daprovin          #+#    #+#             */
-/*   Updated: 2019/10/23 23:31:58 by daprovin         ###   ########.fr       */
+/*   Updated: 2019/10/25 04:08:20 by daprovin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
+typedef struct	s_statlst
+{
+	int fd;
+	char *content;
+	struct s_statlst *next;
+}				t_statlst;
+
+#endif
